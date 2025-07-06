fd51f0816cb44fc03264865a732500dd58d17cab
Mariano Ledo <marianoxvii@gmail.com>
2025-07-06 13:06:02 +0200

    feat: Add linting for typescript

--- lua/mledo/lint.lua
+++ lua/mledo/lint.lua
@@ -4,6 +4,8 @@ vim.api.nvim_create_autocmd({ "BufWritePost" }, {
 
         if string.sub(file, -6) == ".swift" then
             vim.cmd("silent !swiftlint --fix --format " .. file)
+        elseif string.sub(file, -3) == ".ts" then
+            vim.cmd("silent !npx prettier " .. file .. " --write")
         end
     end,
 })
