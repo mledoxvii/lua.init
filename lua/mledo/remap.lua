41ec5c38d19e242bb812f7cb7227349676185951
Mariano Ledo <mlg@trifork.com>
2025-07-12 13:00:18 +0200

    feat: Use arrow keys to move page up and down

--- lua/mledo/remap.lua
+++ lua/mledo/remap.lua
@@ -4,8 +4,8 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")
 
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<Down>", "<C-d>zz")
+vim.keymap.set("n", "<Up>", "<C-u>zz")
 
 vim.keymap.set("n", "<leader>d", "\"_d")
 vim.keymap.set("v", "<leader>d", "\"_d")
